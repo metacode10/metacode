b9d43b9be5a3bf68f5d9479a35bb321994539de3
Yoonsung Kang <metacode10@gmail.com>
2025-03-09 19:29:18 +0900

    a
    a

---
+++ nextworld12/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React, {useState} from 'react';
+
+function App() {
+
+  let post = '강남 우동 맛집';
+  let [ctt,setChange] = useState('남자 코트 추천');
+
+  setChange = () => {
+    '코트 추천';
+  }
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+      블로그임
+      </div>
+      <div className="list">
+        <h4>{ctt} </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <h4>{post}</h4>
+    </div>
+  );
+}
+
+export default App;
