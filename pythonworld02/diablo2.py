1c2aa1ab8bb3fa14ce2e9924e346151c35e71593
signal <metacode10@gmail.com>
2025-03-14 13:33:15 +0900

    a
    a

---
+++ pythonworld02/diablo2.py
@@ -0,0 +1,8 @@
+class Amazon:
+    strength = 20
+    dexterity = 25
+    vitality = 20
+    energy = 15
+
+    def attack(self):
+        return 'Jab!!!'
---
+++ pythonworld02/hello4.py
@@ -0,0 +1,8 @@
+class Amazon:
+    strength = 20
+    sexterity = 25
+    vitality = 20
+    energy = 15
+
+    def attack(self):
+        return 'Jab!!!'
