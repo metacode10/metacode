572466c422479e5ec688f7959721dd2d5e8647ee
signal <metacode10@gmail.com>
2025-03-19 11:17:55 +0900

    a
    b

--- next01/next01/settings.py
+++ next01/next01/settings.py
@@ -37,7 +37,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',    
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication'
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- next01/next01/urls.py
+++ next01/next01/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from example_app.views import hello
+from example_app.views import hello_rest_api
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', hello),
+    # path('hello/', hello),
+    path('api/hello/', hello_rest_api, name='hello_rest_api'),
 ]
